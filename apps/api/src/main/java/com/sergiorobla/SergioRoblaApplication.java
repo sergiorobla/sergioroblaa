7720641eb634cf862bb89507ff22d540c7a11a7c
sergiorobla <sergiorobla36@gmail.com>
2026-03-01 01:37:53 +0100

    feat: initial project setup - phase 1 completed

---
+++ apps/api/src/main/java/com/sergiorobla/SergioRoblaApplication.java
@@ -0,0 +1,13 @@
+package com.sergiorobla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SergioRoblaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SergioRoblaApplication.class, args);
+	}
+
+}
---
+++ apps/api/src/test/java/com/sergiorobla/SergioRoblaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sergiorobla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SergioRoblaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
